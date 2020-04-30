35eb85b6913871ebf9451d69822d0c91c7b1cc83
anegm98 <a.negm98.an@gmail.com>
2020-04-30 20:01:33 +0200

    add command line argument support

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,9 @@
+import argparse
 from hi import *
+
+parser = argparse.ArgumentParser("dsfs")
+parser.add_argument("-a", default=1, help="This is the 'a' variable")
+args = parser.parse_args()
 
 def run():
 	func()
