88d7fabf7917ed46d0a9305fab143ebaec206877
anegm98 <a.negm98.an@gmail.com>
2020-05-02 02:07:22 +0200

    add mapper class

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import argparse
 from decode import *
 from trie import *
 from kmer import *
+from mapper import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", default=1, help="Which method to use? 1:trie 2:suffix array 3: bwd")
@@ -17,10 +18,4 @@ def run():
 		reference_kmer = kmer_maker(args.k, True, reference.seq)
 		sequence_kmer = kmer_maker(args.k, False, sequence.seq)
 		reference_trie = Trie()
-		for i in reference_kmer.kmers:
-			for j in reference_kmer.kmers[i]:
-				reference_trie.add_suffix(j)
-		for i in sequence_kmer.kmers:
-			for j in sequence_kmer.kmers[i]:
-				if reference_trie.find_suffix(j) != -1:
-					print('x')
+		map = mapper(reference_kmer.kmers, sequence_kmer.kmers, reference_trie)
---
+++ src/mapper.py
@@ -0,0 +1,16 @@
+from trie import *
+
+class mapper():
+    def __init__(self, reference, sequence, trie):
+        self.add_reference(reference, trie)
+        self.map_sequence(sequence, trie)
+
+    def add_reference(self, reference, trie):
+        for sequenceName in reference:
+            for kmer in reference[sequenceName]:
+                trie.add_suffix(kmer[0], sequenceName, kmer[1])
+
+    def map_sequence(self, sequence, trie):
+        for sequenceName in sequence:
+            for kmer in sequence[sequenceName]:
+                trie.find_suffix(kmer[0])
