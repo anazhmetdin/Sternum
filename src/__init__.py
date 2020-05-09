2f926f238f530334601894e392146526169192e0
anegm98 <a.negm98.an@gmail.com>
2020-05-09 12:42:05 +0200

    edit parameters in mapper

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ parser.add_argument("-r", default="", help="Reference file path")
 parser.add_argument("-s", default="", help="Sequence reads file path")
 parser.add_argument("-k", default=13, help="k-mer size")
 parser.add_argument("-p", default=-1, help="Patch size")
-parser.add_argument("-g", default=10, help="Min allowed number of matching\
+parser.add_argument("-c", default=10, help="Min allowed count of matching\
  kmers")
 parser.add_argument("-q", default=70, help="Min allowed matching similarity\
  percentage")
@@ -27,4 +27,4 @@ def run():
         seqKmer = kmer_maker(int(args.k), sequence, False)
         reference_trie = Trie()
         sternum = mapper(refKmer, seqKmer, reference_trie, int(args.p))
-        sternum.filter_matching(int(args.g), int(args.q))
+        sternum.filter_matching(int(args.c), int(args.q))
